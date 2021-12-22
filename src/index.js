800aeab09bf7a01507170950931d2989e41712e7
Alejandro-va <prof.jahz@gmail.com>
2021-12-22 10:02:57 -0300

    backend listo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors"); //para conectar back con front
 
 const taskRoutes = require("./routes/tasks.routes");
 
 const app = express();
 
+app.use(cors()); //permite comuiniocar ambos servidores de manera simple
 app.use(morgan("dev"));
 app.use(express.json()); //ed esta forma el servidor va a entender que  la informacin  que va en el contenido del body es json
 
